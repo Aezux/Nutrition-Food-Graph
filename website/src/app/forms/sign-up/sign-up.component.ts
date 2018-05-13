ae45b85b3146a5af3e549509ca109acd9900186f
Adrian <adrianhyc@gmail.com>
2018-05-12 19:29:42 -0500

    form validation

--- website/src/app/forms/sign-up/sign-up.component.ts
+++ website/src/app/forms/sign-up/sign-up.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../user';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
-export class SignUpComponent implements OnInit {
 
-  constructor() { }
+export class SignUpComponent implements OnInit {
+  user: User;
+
+  constructor() {
+    this.user = new User({
+      email: '', 
+      password: ''
+    });
+  }
 
   ngOnInit() {
   }
 
+  onFormSubmit({ value, valid }: { value: User, valid: boolean }) {
+    this.user = value;
+  }
 }
