22a37a5d0d96d5bffa2beb02dd2c56d3370be0cf
Vladislav Yourtchenko <vyourtchenko@gmail.com>
2018-06-05 18:20:58 -0500

    added auth test

---
+++ website/e2e/auth.e2e-spec.ts
@@ -0,0 +1,63 @@
+'use strict';
+
+var expec = require('chai').expect;
+var runInSandbox = require("auth0-rules-testharness");
+
+var user = {
+  "email": "richard.seldon@auth0.com",
+  "email_verified": true,
+  "name": "Richard Seldon"
+};
+
+var context = {
+  "clientID": "wWXS5rz3asdfdfkzbCXho3zNPNv77c",
+  "clientName": "My Auth0 Client",
+  "connection": "MY-DB"
+};
+
+var configuration = {
+  NAME: 'world'
+};
+
+var params = {
+  timeout: 5,
+  ca: '',
+  tenant: 'my-super-tenant',
+  url: 'https://sandbox.it.auth0.com',
+  token: '<webtask-token>'
+};
+
+
+describe('auth0-rules-testharness', function () {
+
+  var ruleScript = `function (user, context, callback) {
+  console.log('hello,', configuration.NAME);
+  user.foo = "bar";
+  callback(null, user, context);
+}`;
+
+  it('should console log "hello, world" - available in output array', function (done) {
+
+    var callback = function (err, result, output, stats) {
+      // console.log('output: ', output);
+      expec(output[0]).to.equal('hello, world');
+      done();
+    };
+
+    var args = [user, context, callback];
+    runInSandbox(ruleScript, args, configuration, params);
+  });
+
+  it('should append attribute "foo" to user with value "bar" - available in result object', function (done) {
+
+    var callback = function (err, result, output, stats) {
+      // console.log('result: ', result);
+      expec(result.foo).to.equal('bar');
+      done();
+    };
+
+    var args = [user, context, callback];
+    runInSandbox(ruleScript, args, configuration, params);
+  });
+
+});
